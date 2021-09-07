fd02821a7a502aa2e4f3699d90ef6bdd822b3781
Gamorski, Piotr <piotr.gamorski@fujitsu.com>
2021-09-07 17:51:13 +0200

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,14 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobileMenu = document.querySelector('.header .nav-bar .nav-list ul');
-const header = document.querySelector('.header .container')
+const listElements = document.querySelectorAll('.header .nav-bar .nav-list ul li');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobileMenu.classList.toggle('active');
+})
+
+listElements.forEach(li => {
+  li.addEventListener('click', () => {
+      mobileMenu.classList.toggle('active');
+  })
 })
